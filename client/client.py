d11ea7efa53395e88c32fdc9599cce331066b793
Gal Shnapp <galshnapp@Gals-Mac-mini.local>
2021-07-09 16:59:49 +0300

    Docerised client Up and running.
    ** client request out going IP should still be taken maunly from production.

--- client/client.py
+++ client/client.py
@@ -1,20 +1,17 @@
 import requests
-
 import time
+import signal
+from sys import argv
 
-BASE = "http://127.0.0.1:5000/"
+BASE = "http://172.17.0.2:5000/"
 
 def single_cycle():
-    response = requests.get(BASE + "msg")
-    print(response.json())
+    try:
+        response = requests.get(BASE + "msg")
+        print(response.json())
+    except:
+        print("exception handling at it's best!")
     time.sleep(1)
-
-
-
-
-
-
-
 
 
 
@@ -22,7 +19,19 @@ def single_cycle():
 #             MAIN               #
 ##################################
 
-USERNAME = input('Username please :)')
+DEBUG = 0
+if len(argv) > 1 and argv[1] == 'debug':
+    DEBUG = 1
+
+
+
+if DEBUG == 1:
+    USERNAME = 'ANON_USER'
+else:
+    USERNAME = input('Username please :)')
+
+print("Hello!, {} you should begin typing your msgs now".format(USERNAME))
+
 
 while True:
     single_cycle()
