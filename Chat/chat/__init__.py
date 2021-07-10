a2fbf24aa443953c7e0ee36fad87d875d1523f4b
Gal Shnapp <galshnapp@Gals-Mac-mini.local>
2021-07-10 16:27:14 +0300

    A brave attaempt

--- Chat/chat/__init__.py
+++ Chat/chat/__init__.py
@@ -1,22 +1,31 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+import os
 
 app = Flask(__name__)
+
 
 ##################################
 #              DB                #
 ##################################
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite+pysqlite:///site.db'
+DB_FILE='site.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite+pysqlite:///{}'.format(DB_FILE)
+app.config['SECRET_KEY'] = 'secret!'
+
 
 db=SQLAlchemy(app)
 
 
 from chat.models import Conversation
 
-# GET DB to preload.
-db.create_all()
+if os.path.exists("{}".format(DB_FILE)):
+    print("it exists, not creating database")
+
+else:# GET DB to preload.
+    db.create_all()
 chat_room = Conversation()
 db.session.add(chat_room)
 db.session.commit()
